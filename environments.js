87a3fbb2b4d7a51073f7852fa563d280730781bd
Philip Tallmadge <philip_tallmadge@philips-mbp.dhcp.nd.edu>
2025-03-04 18:58:30 -0500

    Added environments info

---
+++ environments.js
@@ -0,0 +1,6 @@
+module.export = {
+    APLLICATION_ID: "mzdIgscbWu2MYLyRWsnduoyN2SFxvCLh3rYlUFhV",
+    JAVASCRIPT_KEY: "uHBF9SILFnsQDnG8vCUScT6hSrBlrYMy3p9kE00P",
+    SERVER_URL: "https://parseapi.back4app.com",
+  };
+  
