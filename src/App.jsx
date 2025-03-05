47c55a1208b375b7419c81e92e361451927ebe92
Philip Tallmadge <philip_tallmadge@philips-mbp.dhcp.nd.edu>
2025-03-05 15:14:39 -0500

    Moved environments.js, adding parse

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as Env from "./environments.js"
+import Parse from "parse"
+
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
 
 /*const Env = {
   App id= ...
