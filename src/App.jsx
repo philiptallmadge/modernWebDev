26e55d5e82e16b9d634890ce3eb925560f14f23c
Philip Tallmadge <philip_tallmadge@philips-mbp.dhcp.nd.edu>
2025-03-05 16:46:41 -0500

    Updated jsx to use components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import * as Env from "./environments.js"
-import Parse from "parse"
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Components from "./Components/Components.js";
+import * as Env from "./environments.js";
+import Parse from "parse";
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 Parse.serverURL = Env.SERVER_URL;
@@ -13,33 +14,6 @@ Parse.serverURL = Env.SERVER_URL;
 }
 */
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+  return <Components />;
 }
-
-export default App
