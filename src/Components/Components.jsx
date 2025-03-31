c4566299b5efc895a31e3c99ae74af0062f11a45
Philip Tallmadge <philip_tallmadge@philips-mbp.dhcp.nd.edu>
2025-03-30 22:58:01 -0400

    Updating routing

--- src/Components/Components.jsx
+++ src/Components/Components.jsx
@@ -4,8 +4,13 @@ import Shared from "./Shared/Shared.jsx";
 import Bands from "./Bands/Bands.jsx"; 
 import Venues from "./Venues/Venues.jsx"; 
 //Import React Router for handling navigation
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
+import AuthModule from "./Auth/Auth.js";
+import AuthRegister from "./Auth/AuthRegister";
+import AuthLogin from "./Auth/AuthLogin";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute.js";
+import MainList from "./Main/MainList.js";
 
 //main routing structure for app
 export default function Components() {
@@ -17,6 +22,11 @@ export default function Components() {
           <Route path="/bands" element={<Bands />} />
           <Route path="/venues" element={<Venues />} />
           <Route path="/shared" element={<Shared />} />
+          <Route path="/auth" element={<AuthModule />} />
+          <Route path="/auth/register" element={<AuthRegister />} />
+          <Route path="/auth/login" element={<AuthLogin />} />
+          
+          <Route path="*" element={<Navigate to="/auth" replace />} />
         </Routes>
       <Shared />
     </Router>
